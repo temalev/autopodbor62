990f847fd8cf365f831c77f912fb89dfa5d4d27a
Artem <myartoym@gmail.com>
2026-01-20 01:46:14 +0300

    fix baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['/assets/styles/main.scss']
+  css: ['~/assets/styles/main.scss'],
+  app: {
+    baseURL: '/',
+  }
+
 })
