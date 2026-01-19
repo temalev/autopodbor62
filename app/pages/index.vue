b38507187c0220f7f304cdf9ba1ce9a8f5b79023
Artem <myartoym@gmail.com>
2026-01-20 01:57:15 +0300

    fix styles

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,9 +2,9 @@
   <div class="page">
     <div class="page__content">
       <h1 class="page__title">
-        Автоподбор 62 — покупка<br>
-        и продажа автомобиля без&nbsp;риска
-        и потери времени
+        Автоподбор 62 — покупка
+        и&nbsp;продажа&nbsp;автомобиля без&nbsp;риска
+        и&nbsp;потери времени
       </h1>
       <p class="page__text">
         Мы берём на себя все сложные и рискованные этапы, чтобы вы получили честный автомобиль по реальной цене — без обмана, сюрпризов и лишних затрат.
@@ -34,9 +34,9 @@
 
   &__title {
     color: #fff;
-    font-size: 48px;
+    font-size: 38px;
     font-weight: bold;
-    line-height: 1.4;
+    line-height: 1.2;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
     margin: 100px 0 30px 0;
   }
