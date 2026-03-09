97256263b5596941d8e625a1193df18b69c7a230
temalev <temalev@users.noreply.github.com>
2026-03-09 19:41:52 +0000

    deploy: 39139c639ce004c1124ba2abd35e8f41cb971458

---
+++ _nuxt/BrKwbRfp.js
@@ -0,0 +1,1 @@
+import{a1 as a,a2 as s,a3 as r,I as u,a4 as o}from"./B3dD4A3N.js";function d(t){const e=t||s();return e.ssrContext?.head||e.runWithContext(()=>{if(r()){const n=u(o);if(!n)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return n}})}function i(t,e={}){const n=e.head||d(e.nuxt);return a(t,{head:n,...e})}export{i as u};
