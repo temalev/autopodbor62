94df7cb2f5369445f4b253ff2012f864c0c94733
Artem <myartoym@gmail.com>
2026-05-04 21:41:40 +0300

    Add favicon link to nuxt.config.ts for improved branding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,7 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ],
       meta: [
         { property: 'og:type', content: 'website' },
